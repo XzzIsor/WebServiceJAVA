832c47172003fee74e3c016117d8ea51ab45a466
JUAN ESTEBAN <JUAN ESTEBAN@LAPTOP-TFJQT04S>
2020-11-03 19:55:15 -0500

    Update 3/11

---
+++ RestEDD/src/main/java/com/edd/ws/rest/vo/VOResponse.java
@@ -0,0 +1,14 @@
+package com.edd.ws.rest.vo;
+
+public class VOResponse {
+	String response;
+
+	public String getResponse() {
+		return response;
+	}
+
+	public void setResponse(String response) {
+		this.response = response;
+	}
+	
+}
